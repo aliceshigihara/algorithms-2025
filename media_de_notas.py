814ef88e0a766ef6ad403a6215fa6341838bbd69
Alice Shigihara <aliceshigihara@gmail.com>
2025-03-07 16:21:34 -0300

    media_de_notas.py

--- media_de_notas.py
+++ media_de_notas.py
@@ -1,8 +1,13 @@
 A= float(input()) * 2
+#2: change for your grade
 B= float(input()) * 3
+#3: change for your grade
 C= float(input()) * 5
+#5: change for your grade
 
 soma = A + B + C
 media = soma / 10
+#change 10 for the max grade
 
 print(f"MEDIA = {media:.1f}")
+#change .1f if you want more decimal
