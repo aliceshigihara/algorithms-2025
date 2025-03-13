a0aced1490932e54125be1cf7a9c69d113c18184
Alice Shigihara <aliceshigihara@gmail.com>
2025-03-13 14:44:31 -0300

    Add files via upload

---
+++ test_random.py
@@ -0,0 +1,6 @@
+#biblioteca para os números aleatórios
+import random
+#crie uma variável para os valores de número random
+i = random.randint(0,10)
+#imprima o número random escolhido entre 0 à 10
+print("seu número random é:",i)
