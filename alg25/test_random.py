152db3ae91206cbb5f230d6f9a46a7dc3ed884d6
Alice Shigihara <aliceshigihara@gmail.com>
2025-03-20 17:41:47 -0300

    Delete alg25 directory

--- alg25/test_random.py
+++
@@ -1,6 +0,0 @@
-#biblioteca para os números aleatórios
-import random
-#crie uma variável para os valores de número random
-i = random.randint(0,10)
-#imprima o número random escolhido entre 0 à 10
-print("seu número random é:",i)
