266800bf70f23d064d8130fb105f2258eb457383
Alice Shigihara <aliceshigihara@gmail.com>
2025-03-30 14:59:26 -0300

    Update TRIANGULOS_1045.py

--- beecrowd/TRIANGULOS_1045.py
+++ beecrowd/TRIANGULOS_1045.py
@@ -1,27 +1,31 @@
-C= float(input())
-B= float(input())
-A= float(input())
+valores = input().split()
+valores = list(map(float,valores))
 
-if (B+C) <= A:
+A,B,C = sorted(valores)[::-1]
+
+continua = True
+
+if A >= B+C:
     print("NAO FORMA TRIANGULO")
+    continua = False
 
-if A*A == (B*B + C*C):
+if A**2 == (B**2 + C**2) and continua:
     print("TRIANGULO RETANGULO")
 
-if A*A > (B*B + C*C):
+if A**2 > B**2 + C**2 and continua:
     print("TRIANGULO OBTUSANGULO")
 
-if A*A < (B*B + C*C):
+if A*A < B*B + C*C and continua:
     print("TRIANGULO ACUTANGULO")
 
-if A == B == C:
+if A == B == C and continua:
     print("TRIANGULO EQUILATERO")
 
-if A == B < C:
+if A == B < C and continua:
     print("TRIANGULO ISOCELES")
 
-if A == C < B:
+if A == C < B and continua:
     print("TRIANGULO ISOCELES")
 
-if C == B < A:
+if C == B < A and continua:
     print("TRIANGULO ISOCELES")
