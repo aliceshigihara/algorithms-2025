43d5c7891782866737e661612e15cf7131c1c601
Alice Shigihara <aliceshigihara@gmail.com>
2025-05-05 19:52:04 -0300

    Create 1052 MONTH.py

---
+++ beecrowd/1052 MONTH.py
@@ -0,0 +1,37 @@
+MonthNumber = int(input())
+
+if MonthNumber == 1:
+    print("January")
+
+elif MonthNumber == 2:
+    print("February")
+
+elif MonthNumber == 3:
+    print("March")
+
+elif MonthNumber == 4:
+    print("April")
+
+elif MonthNumber == 5:
+    print("May")
+
+elif MonthNumber == 6:
+    print("June")
+
+elif MonthNumber == 7:
+    print("July")
+
+elif MonthNumber == 8:
+    print("August")
+
+elif MonthNumber == 9:
+    print("September")
+
+elif MonthNumber == 10:
+    print("October")
+
+elif MonthNumber == 11:
+    print("November")
+
+elif MonthNumber == 12:
+    print("December")
