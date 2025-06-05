7a6a5d46cec4ce13475f0a75c206d0f6b4848f4e
Alice <aliceshigihara@gmail.com>
2025-06-05 13:57:17 -0300

    teste

---
+++ pyautogui/pyautogui.configs/button_click.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+pyautogui.confirm('test', buttons= ['Ok','Cancel'])
+
+pyautogui.alert('test')
+
+pyautogui.prompt('test')
---
+++ pygame/pygame_basic_initial.py
@@ -0,0 +1,25 @@
+import pygame
+
+# inicialização do pygame
+pygame.init()
+
+# configuração da tela
+screen = pygame.display.set_mode((1280,720))
+
+# variável de relógio
+clock = pygame.time.Clock()
+
+# repetição contínua
+while True:
+
+    # tratamento de finalização do programa
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+
+    # pintar a tela de roxo
+    screen.fill("blue")
+
+    # atualizar a tela
+    pygame.display.flip()
