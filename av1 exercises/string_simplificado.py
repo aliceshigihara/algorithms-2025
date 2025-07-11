bd35040b144b5d38a8e8213db6218b8efa5171d4
Alice <aliceshigihara@gmail.com>
2025-07-10 23:00:26 -0300

    removendo pois há outra pasta

--- av1 exercises/string_simplificado.py
+++
@@ -1,5 +0,0 @@
-#peça uma string (a)
-a = input("Dê uma string: ") #a = input()
-#percorre "a" de trás pra frente (i)
-for i in range(len(a)-1, -1, -1):       
-    print(a[i], end="") #imprime a[i]
